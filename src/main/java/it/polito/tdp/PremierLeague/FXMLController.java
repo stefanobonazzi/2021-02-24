b009680c6ad2cbbd8be61408222e0a3580401733
stefanobonazzi <stefanobonazzi@mbp-di-stefano.lan>
2022-06-19 16:41:27 +0200

    Little changes in Controller

--- src/main/java/it/polito/tdp/PremierLeague/FXMLController.java
+++ src/main/java/it/polito/tdp/PremierLeague/FXMLController.java
@@ -47,20 +47,42 @@ public class FXMLController {
 
     @FXML
     void doCreaGrafo(ActionEvent event) {
-    	this.model.creaGrafo(cmbMatch.getValue());
-    	txtResult.setText("Vertici: "+this.model.getGraph().vertexSet().size()+"\nArchi: "+this.model.getGraph().edgeSet().size());
+    	Match match = cmbMatch.getValue();
+    	if(match == null) {
+    		txtResult.setText("Seleziona un match!");
+    		return;
+    	}
+    	
+    	this.model.creaGrafo(match);
+    	txtResult.setText("Grafo creato\n#VERTICI: "+this.model.getGraph().vertexSet().size()+"\n#ARCHI: "+this.model.getGraph().edgeSet().size());
     }
 
     @FXML
-    void doGiocatoreMigliore(ActionEvent event) {    	
+    void doGiocatoreMigliore(ActionEvent event) { 
+    	if(this.model.getGraph() == null) {
+    		txtResult.setText("Seleziona un match e crea il grafo prima!");
+    		return;
+    	}
+    	
     	txtResult.setText("Best player:\n"+this.model.bestPlayer());
     }
     
     @FXML
     void doSimula(ActionEvent event) {
-    	int n = Integer.parseInt(txtN.getText());
-    	String s = this.model.simula(n, cmbMatch.getValue());
-
+    	int n;
+    	try {
+    		n = Integer.parseInt(txtN.getText());
+		} catch (Exception e) {
+			txtResult.setText("Inserisci un valore intero!");
+			return;
+		}
+    	Match match = cmbMatch.getValue();
+    	if(match == null) {
+    		txtResult.setText("Seleziona un match!");
+    		return;
+    	}
+    	
+    	String s = this.model.simula(n, match);
     	txtResult.setText(s);
     }
 
